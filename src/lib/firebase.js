cf9065e4d9e4c74757883d29529b8a99efdc4fad
ChannelF-Oleo <channelf.oleo@gmail.com>
2025-11-29 19:09:06 -0400

    Optimización Proyectos: Modal integrado y carga dinámica desde Firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,14 +10,15 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDYybqd55WwWSjJ5__FGNTkhSNqDyk1SVE",
-  authDomain: "portafolio-cfd.firebaseapp.com",
-  projectId: "portafolio-cfd",
-  storageBucket: "portafolio-cfd.firebasestorage.app",
-  messagingSenderId: "488773298734",
-  appId: "1:488773298734:web:d33606301602a3e4794dbf",
-  measurementId: "G-5BY29E59CE",
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENTID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
